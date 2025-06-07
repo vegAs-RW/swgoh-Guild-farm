a55cb47b33b16d51c7e6fd28cd47f0bbaa400984
unknown <jordan.pouchot2@gmail.com>
2025-06-07 18:24:54 +0200

    :sparkles: Feature for fetch players data

---
+++ players.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const getPlayerData = async (allyCode: string) : Promise<any | null> => {
+    try {
+        const res = await axios.get(`https://swgoh/api/player/${allyCode}`)
+        return res.data.units;
+    } catch (error) {
+        console.error (`Fail to get data for ${allyCode} :`, error)
+        return null
+    }
+}
